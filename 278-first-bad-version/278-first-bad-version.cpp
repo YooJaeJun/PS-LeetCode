dce57dd803ea796104868570ba173ddd9c6b9108
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-03-16 11:56:32 +0900

    Time: 4 ms (24.62%), Space: 5.9 MB (23.48%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long start = 1;
+        long long end = n;
+        while (start <= end) {
+            long long mid = (start + end) / 2;
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+};
