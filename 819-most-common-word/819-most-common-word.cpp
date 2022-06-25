20dc31b01e22088ec3514006477d5844c3be0699
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-06-26 02:41:56 +0900

    Time: 3 ms (93.33%), Space: 7.9 MB (33.09%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        for(auto& elem : paragraph)
+        {
+            if (elem >= 'A' and elem <= 'Z') elem -= 'A' - 'a';
+            else if (elem >= 'a' and elem <= 'z') {}
+            else elem = ' ';
+        }
+
+        map<string, int> dic;
+        for(auto& elem : banned)
+            dic[elem] = -1;
+        
+        stringstream ss(paragraph);
+        string str;
+        while(ss >> str)
+            if (dic[str] != -1) 
+                dic[str]++;
+        
+        int maxn = 0;
+        string ans;
+        for(auto& elem : dic)
+        {
+            if (maxn < elem.second)
+            {
+                maxn = elem.second;
+                ans = elem.first;
+            }
+        }
+        
+        return ans;
+    }
+};
